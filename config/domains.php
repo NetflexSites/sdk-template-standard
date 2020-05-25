45a897ce2705923d649a13c4cdf8a2e548365ed6
Thomas Alrek <thomas@alrek.no>
2020-05-25 20:56:12 +0200

    Create domains.php

---
+++ config/domains.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Domain
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the default mock domain used in development
+    |
+    */
+
+    'default' => env('DEV_DOMAIN', null),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Mappings
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the mock domain mappings
+    |
+    */
+
+    'mappings' => [
+        // 'localhost' => 'example.com',
+    ],
+];
