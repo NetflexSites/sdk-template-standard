1ee50872a5d67402945cddcc9f33cec9ebe5ba2f
Olav Øyen <olav@apility.no>
2020-12-08 00:06:45 +0100

    New mix config with dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,52 @@
-const mix = require('laravel-mix');
+/* eslint-disable @typescript-eslint/no-var-requires */
+const mix = require('laravel-mix')
+const path = require('path')
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+require('laravel-mix-eslint')
+require('laravel-mix-polyfill')
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.autoload({
+  jquery: ['$', 'jQuery']
+});
+
+mix
+  .js('resources/js/app.js', 'public/js')
+  .eslint()
+  .sass('resources/sass/app.scss', 'public/css')
+  .polyfill({
+    useBuiltIns: 'entry',
+    targets: false,
+  })
+  .webpackConfig({
+    devServer: {
+      serveIndex: true,
+      before (app, server) {
+        server._watch([
+          'app/**/*.php',
+          'resources/views/**/*.php',
+        ])
+      },
+    },
+  })
+  .extract()
+  .disableNotifications()
+  .options({
+    processCssUrls: false,
+    postCss: [
+      require('postcss-logical')({ dir: 'ltr' }),
+    ],
+    autoprefixer: {
+      enabled: true,
+      options: {
+        grid: true,
+      },
+    },
+  })
+
+if (mix.inProduction()) {
+  mix
+    .version()
+} else {
+  mix
+    .sourceMaps()
+}
