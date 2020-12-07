5475844f85611bdd6403bc58504cd154e7a75e3b
Olav Øyen <olav@apility.no>
2020-12-08 00:09:43 +0100

    Updating base to include mix components and seo

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -2,10 +2,14 @@
 <html>
 <head>
   @yield('head')
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <x-seo />
+  <x-mix-head />
 </head>
 <body>
   @yield('body')
   @stack('scripts')
+  <x-mix-body />
   <x-editor-tools />
 </body>
 </html>
