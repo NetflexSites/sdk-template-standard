571abd99c7ad9adf5567e5e5ac622e098b43bf20
Thomas Alrek <thomas@alrek.no>
2021-03-22 11:24:34 +0100

    Create pages.php

---
+++ config/pages.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Pages
+    |--------------------------------------------------------------------------
+    |
+    | Configuration for Netflex Pages
+    |
+    */
+    
+    'controller' => Netflex\Pages\Controllers\PageController::class,
+
+    'prefix' => 'netflex',
+
+    'components' => [
+        Netflex\Pages\Components\BackgroundImage::class,
+        Netflex\Pages\Components\Blocks::class,
+        Netflex\Pages\Components\EditorButton::class,
+        Netflex\Pages\Components\EditorTools::class,
+        Netflex\Pages\Components\Image::class,
+        Netflex\Pages\Components\Inline::class,
+        Netflex\Pages\Components\Nav::class,
+        Netflex\Pages\Components\Picture::class,
+        Netflex\Pages\Components\Seo::class,
+        Netflex\Pages\Components\StaticContent::class,
+    ],
+
+];
